c3854e5e8596fd9dcdd98451f6afb3cb792d6388
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-03-07 00:22:23 +0500

    Toggling window visibility

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const electron = require("electron");
+const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
-let mainBrowserWindow;
+let mainBrowserWindow, tray;
 
 app.on("ready", () => {
   mainBrowserWindow = new BrowserWindow({
@@ -11,9 +12,25 @@ app.on("ready", () => {
     },
     frame: false,
     height: 500,
+    show: false,
     resizable: false,
     width: 300
   });
 
   mainBrowserWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  const iconName =
+    process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
+
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
+  tray = new Tray(iconPath);
+
+  tray.on("click", () => {
+    if (mainBrowserWindow.isVisible()) {
+      mainBrowserWindow.hide();
+    } else {
+      mainBrowserWindow.show();
+    }
+  });
 });
